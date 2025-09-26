356393b4203df85cd24409d7e67d14d817098b5f
Adriana-mcz <i202518301@cibertec.edu.pe>
2025-09-26 03:15:57 +0000

    ejercicios de práctica

---
+++ 06_app.py
@@ -0,0 +1,15 @@
+# Calcular el monto de una compra de equipo informático
+
+# Inicio: Crear las variables
+producto = "Laptop"
+precio_unitario = 2500
+cantidad = 2
+
+# Proceso: Realizar las operaciones de cálculo
+monto_total = precio_unitario * cantidad
+
+# Salida: Visualización de resultados
+print(f"Producto: {producto}")
+print(f"Precio unitario: S/{precio_unitario}")
+print(f"Cantidad:{cantidad}")
+print(f"Monto total a pagar: S/{monto_total}")
